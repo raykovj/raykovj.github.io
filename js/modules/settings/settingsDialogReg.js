48fb98b3d2de9677e7c38d097d97af0224ef160c
Jordan Raykov <raykov.jordan@gmail.com>
2021-09-03 17:29:51 -0600

    210903

--- js/modules/settings/settingsDialogReg.js
+++
@@ -1,6 +0,0 @@
-define(['knockout'], function(ko) {
-	ko.components.register('settings-dialog', {
-	    viewModel: { require: 'modules/settings/settingsDialog'},
-	    template: { require: 'text!modules/settings/settingsDialogContent.html' }
-	});
-});
